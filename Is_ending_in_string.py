814b2349273cf9312867fa940b8ea584946cbd94
catrionafsmith <113362369+catrionafsmith@users.noreply.github.com>
2022-11-21 11:19:40 +0000

    "Reflection updated on this challenge"

--- Is_ending_in_string.py
+++ Is_ending_in_string.py
@@ -1,6 +1,13 @@
 # Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
+########
+# My thoughts:
+# Check positions from end - use negative indexes
+# Only need to check for len(ending) chars
+# Could reverse the strings - would that help? - use a slice that steps backwards string[::-1]
 
+
+# My solution:
 def solution(string, ending):
     if ending == '':
         return True
@@ -14,10 +21,11 @@ def solution(string, ending):
             return False
         return True
 
-#Best solution
+#Best solution - there is a python function endswith (and also startswith)!
 def solution(string, ending):
     return string.endswith(ending)
 
-#or
+#or checking if the ending is in a string slice, based on the length of the ending. N.B. If the ending is longer than the string
+# True will not be returned.
 def solution(string, ending):
     return ending in string[-len(ending):]
