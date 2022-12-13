64fff226de0a54528d19994bf1d7724e0e3b1926
catrionafsmith <113362369+catrionafsmith@users.noreply.github.com>
2022-12-13 11:14:52 +0000

    Add new coding challenges - advent and JS

---
+++ 202211_stringSmash.js
@@ -0,0 +1,7 @@
+// 8kyu Write a function that takes an array of words and smashes them together into a sentence and returns the sentence.
+
+function smash (words) {
+    return words.join(" ")
+ };
+
+//  This was the suggested solution! I have ranked up to level 7kyu in JS!
